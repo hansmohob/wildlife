769aa8c177c3db67c054c068921da0b7a3c43293
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2025-07-06 17:20:02 +0100

    typo

--- workshop/devbox-setup.sh
+++ workshop/devbox-setup.sh
@@ -377,8 +377,8 @@ perl -i -pe "BEGIN{undef $/;}
     s/REPLACE_PUBLIC_SUBNET_1/${PUBLIC_SUBNET_1}/g; 
     s/REPLACE_PUBLIC_SUBNET_2/${PUBLIC_SUBNET_2}/g; 
     s/REPLACE_SECURITY_GROUP_APP/${SECURITY_GROUP_APP}/g; 
-    s/REPLACE_SECURITY_GROUP_ALB/${SECURITY_GROUP_ALB}/g;"
-    s/REPLACE_S3_BUCKET_WILDLIFE/${S3_BUCKET_WILDLIFE}/g;" \
+    s/REPLACE_SECURITY_GROUP_ALB/${SECURITY_GROUP_ALB}/g;
+    s/REPLACE_S3_BUCKET_WILDLIFE/${S3_BUCKET_WILDLIFE}/g" \
     $(find /home/ec2-user/workspace/my-workspace \
         -name "task_definition_*.json" -o \
         -name "menu-start.sh")
