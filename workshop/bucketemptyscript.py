7b9fe8af244b5c659b8f91fc5e3b1177bdae42e0
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2025-09-03 11:18:12 +0100

    vdfvdf

--- workshop/bucketemptyscript.py
+++ workshop/bucketemptyscript.py
@@ -19,10 +19,3 @@ for bucket_name in BUCKETS:
         print(f"Deleted all objects from S3 bucket: {bucket_name}")
     except Exception as e:
         print(f"Error deleting objects from S3 bucket {bucket_name}: {e}")
-
-
-
-           s/REPLACE_S3_BUCKET_WILDLIFE/${S3_BUCKET_WILDLIFE}/g;
-    s/REPLACE_S3_BUCKET_LOGS/${S3_BUCKET_LOGS}/g;
-    s/REPLACE_S3_BUCKET_GIT/${S3_BUCKET_GIT}/g;
-    s/REPLACE_S3_BUCKET_ARTIFACT/${S3_BUCKET_ARTIFACT}/g;
