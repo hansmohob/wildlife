5a98d3dbed99be311e561e158e6ec2d0344a7779
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2025-09-12 08:16:26 +0100

    deleted fixed code

--- terraform-live/fixed/09-s3-storage.tf
+++
@@ -1,6 +0,0 @@
-# IAM Policy Attachments for Application
-
-resource "aws_iam_role_policy_attachment" "ecs_task_s3_policy" {
-  role       = data.aws_iam_role.ecs_task.name
-  policy_arn = data.aws_iam_policy.application_data_policy.arn
-}
